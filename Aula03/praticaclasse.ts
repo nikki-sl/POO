614ab398793a1513745723da9dbde698e238712d
nikki-sl <nicolesousa1306@gmail.com>
2023-09-12 23:56:49 -0300

    questão pratica

--- Aula03/praticaclasse.ts
+++ Aula03/praticaclasse.ts
@@ -1,36 +1,30 @@
-// Crie uma classe Animal com os atributos especie e barulho.
-// Crie um construtor que receba esses dois atributos e os inicialize.
-// Crie um método void fazerBarulho que imprima o barulho do animal.
-// Crie os objetos cachorro, gato e vaca com os respectivos barulhos.
-// Chame os métodos fazerBarulho de cada um dos objetos.
-// Crie um objeto da espécie gato, mas que faça o barulho miovisk e salve numa variável chamada gatoRusso.
-// Crie um objeto da especie gatoZumbi, mas que faça o barulho meeh e salve numa variável chamada gatoZumbi.
-// Adicione um atributo alive na classe Animal e inicialize com true.
-// Crie um método void morrer que altere o atributo alive para false, a não ser que o animal seja um gatoZumbi, porque gatos zumbis não morrem.
+/*
+O que foi feito?
+  foi criado uma classe de animais onde eles fazem barulhos e acabam morrendo
 
-// animal especie barulho
-// class Animal {
-//     especie: string;
-//     barulho: string;
-//     alive: true;
+Com quem fez?
+  Eu estava fazendo de certa forma só mas quando eu empacava em algo que não sabia em específico pedi ajuda de amigos programadores que me ajudassem
 
-//     constructor(especie, barulho) {
-//         this.especie = "especie";
-//         this.barulho = "barulho"
+Quanto tempo levou?
+  Acho que foi umas 2h pois era meio intercalado
 
-//     }
+O que aprendeu?
+  praticamente tudo que foi feito na questão eu não sabia, apanhei muito pro código porque eu sabia que estava faltando certas coisas para dar certo, mas não conseguia sair do canto, mas o constructor bom de se usar, os nomes me confundiram muito e tava com um pouco de dificuldade de onde colocar certas coisas mas acho que com a atividade tá sendo melhor entender, mas acho que mais pra frente vou tentar refazer e revisar novamente.
 
-//     fazerBarulho(): void {
-//         console.log(this.barulho)
-//     }
-// }
 
-// // juntando a especie com o barulho
+COMANDO DA QUESTÂO
 
-// const gato = new Animal('gato', 'miau');
-// const cachorro = new Animal('cachorro', 'au au');
+Crie uma classe Animal com os atributos especie e barulho.
+Crie um construtor que receba esses dois atributos e os inicialize.
+Crie um método void fazerBarulho que imprima o barulho do animal.
+Crie os objetos cachorro, gato e vaca com os respectivos barulhos
+Chame os métodos fazerBarulho de cada um dos objetos.
+Crie um objeto da espécie gato, mas que faça o barulho miovisk e salve numa variável chamada gatoRusso.
+Crie um objeto da especie gatoZumbi, mas que faça o barulho meeh e salve numa variável chamada gatoZumbi.
+Adicione um atributo alive na classe Animal e inicialize com true.
+Crie um método void morrer que altere o atributo alive para false, a não ser que o animal seja um gatoZumbi, porque gatos zumbis não morrem.
 
-// gato.fazerBarulho();
+*/
 
 class Animal {
   especie: string;
@@ -48,29 +42,24 @@ class Animal {
   }
 
   morrer(): void {
-    if (this.especie !== 'gatoZumbi') {
+    if(this.especie === "gatoZumbi"){
+      console.log("gato zumbi não morre");
+    } else {
       this.alive = false;
     }
   }
 }
 
-const cachorro = new Animal('cachorro', 'au au');
 const gato = new Animal('gato', 'miau');
-const vaca = new Animal('vaca', 'moo');
-
-cachorro.fazerBarulho();
-gato.fazerBarulho();
-vaca.fazerBarulho();
+const cachorro = new Animal('cachorro', 'au au');
+const vaca = new Animal('vaca', 'muu');
 
 const gatoRusso = new Animal('gato', 'miovisk');
 const gatoZumbi = new Animal('gatoZumbi', 'meeh');
 
+gato.fazerBarulho();
+cachorro.fazerBarulho();
+vaca.fazerBarulho();
 gatoRusso.fazerBarulho();
 gatoZumbi.fazerBarulho();
-
-gatoRusso.morrer();
 gatoZumbi.morrer();
-
-console.log(`O gatoRusso está vivo? ${gatoRusso.alive}`);
-console.log(`O gatoZumbi está vivo? ${gatoZumbi.alive}`);
-  
